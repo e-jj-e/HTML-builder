df3fea94a54bf3ba3d7d8f57bab6f0ca3ac4dfc1
ejj <AkoshPshon@yandex.com>
2022-05-24 16:48:18 +0600

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+let listOfFiles = './04-copy-directory/files';
+let directory = './04-copy-directory/';
+ 
 
+getCurrentFilenames();
+ 
+fs.mkdir(path.join(directory, 'files-copy'),
+  { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Directory created successfully!');
+  });
+
+
+// const srcPath = path.join(__dirname);
+// console.log(srcPath);
+function getCurrentFilenames() {
+  fs.readdir(listOfFiles, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+    //   console.log('\nCurrent directory filenames:');
+      files.forEach(file => {
+        // console.log(file);
+        let readableStream = fs.createReadStream(__dirname + '/files/' + `${file}`);
+
+        let writeableStream = fs.createWriteStream('./04-copy-directory/files-copy/' + `${file}`);
+ 
+        readableStream.on('data', function(chunk){
+          writeableStream.write(chunk);
+
+  
+        });
+      });
+    }
+  });
+}
